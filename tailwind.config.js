c85060205b926fc640e0ef04eaad234db02ddd9e
Fikri Fauzi <110084024+Yoru017@users.noreply.github.com>
2023-09-04 21:52:20 +0700

    fikri-porto-web

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode:'class',
+  theme: {
+    extend: {
+      colors: {
+        main: '#0ea5e9',
+        dark: '#0f172a'
+      }
+    },
+  },
+  plugins: [],
+}
+
